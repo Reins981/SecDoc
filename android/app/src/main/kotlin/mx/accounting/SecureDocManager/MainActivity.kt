1c04b471939fd95ee73a408dfb5b581e7cda0ed6
reko8680 <reins981@gmail.com>
2023-08-25 16:27:56 +0200

    New Layout and Pacakges

--- android/app/src/main/kotlin/mx/accounting/SecureDocManager/MainActivity.kt
+++ android/app/src/main/kotlin/mx/accounting/SecureDocManager/MainActivity.kt
@@ -1,6 +1,11 @@
 package bacqroo.mx.sec_doc
 
 import io.flutter.embedding.android.FlutterActivity
+import androidx.multidex.MultiDexApplication
 
 class MainActivity: FlutterActivity() {
 }
+class MainApplication : MultiDexApplication() {
+    // ...
+}
+
