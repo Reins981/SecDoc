a74abfbf408726229c81d19e849859bc2fd8a57c
reko8680 <reins981@gmail.com>
2023-08-25 14:56:37 +0200

    Login draft

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 
+const TextStyle loginButtonTextStyle = TextStyle(
+  fontSize: 20,
+  // Other properties...
+);
+
 void main() {
   runApp(const MyApp());
 }
@@ -12,7 +17,16 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Accounting App',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.blue,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
+            ),
+            textStyle: loginButtonTextStyle,
+            minimumSize: const Size(double.infinity, 60),
+          ),
+        ),
       ),
       home: const LoginScreen(),
     );
@@ -29,24 +43,39 @@ class LoginScreen extends StatelessWidget {
         title: const Text('Login'),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const TextField(
-              decoration: InputDecoration(labelText: 'Email'),
-            ),
-            const TextField(
-              obscureText: true,
-              decoration: InputDecoration(labelText: 'Password'),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                // TODO: Implement login logic
-              },
-              child: const Text('Login'),
-            ),
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/logo.jpg', // Replace with your logo asset path
+                width: 120, // Adjust the width as needed
+              ),
+              const SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Column(
+                  children: [
+                    const TextField(
+                      decoration: InputDecoration(labelText: 'Email'),
+                    ),
+                    const SizedBox(height: 10),
+                    const TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(labelText: 'Password'),
+                    ),
+                    const SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed: () {
+                        // TODO: Implement login logic
+                      },
+                      child: const Text('Login'),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
