e58f08567a7e0f281c5f681bd7f4ea346b6cffdb
reko8680 <reins981@gmail.com>
2023-08-26 00:18:30 +0200

    Document library

--- lib/document_library_screen.dart
+++ lib/document_library_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:path_provider/path_provider.dart';
 import 'main.dart'; // Import the LoginScreen to navigate back after logout
+import 'package:http/http.dart' as http;
+import 'dart:io';
 
 void main() {
   runApp(const MyApp());
@@ -213,8 +216,34 @@ class _DocumentLibraryScreenState extends State<DocumentLibraryScreen> {
   void downloadDocument(Map<String, dynamic> documentData) {
     // Implement your download logic here
     // Use the 'documentData' variable to identify the selected document and initiate the download
-    print("aaaaaaaaaaaaa");
+    print("Download initiated for document:");
     print(documentData);
+    final documentName = documentData['document_name'];
+    final downloadUrl = documentData['document_url'];
+
+    // Perform the actual download using the provided URL
+    // You can use libraries like 'http' or 'dio' for making network requests
+    // Example:
+    downloadFunction(downloadUrl, documentName);
+  }
+  void downloadFunction(String downloadUrl, String documentName) async {
+    // Use the 'http' package to initiate the download
+    final response = await http.get(Uri.parse(downloadUrl));
+    if (response.statusCode == 200) {
+      // Save the downloaded file to local storage or display the downloaded content
+      // You can use libraries like 'path_provider' to manage local files
+      // Example:
+      final appDocumentsDirectory = await getApplicationDocumentsDirectory();
+      final filePath = '${appDocumentsDirectory.path}/$documentName.pdf';
+      final file = File(filePath);
+      await file.writeAsBytes(response.bodyBytes);
+
+      print("Download completed for $documentName");
+      print("File saved at: $filePath");
+    } else {
+      print("Download failed for $documentName");
+      print(response.statusCode);
+    }
   }
 }
 
@@ -266,3 +295,4 @@ class DocumentDetailScreen extends StatelessWidget {
     );
   }
 }
+
